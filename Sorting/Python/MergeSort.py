e664c975c9dbffa895946e733d193c2887e40a0f
Huakun Shen <shenhuakun@outlook.com>
2019-05-08 23:26:33 -0400

    added python merge sort

---
+++ Sorting/Python/MergeSort.py
@@ -0,0 +1,37 @@
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return
+    mid = len(arr) // 2
+    left = arr[:mid]
+    right = arr[mid:]
+
+    merge_sort(left)
+    merge_sort(right)
+
+    merge(arr, left, right)
+
+
+def merge(arr, left, right):
+    i = j = num = 0
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            arr[num] = left[i]
+            i += 1
+        else:
+            arr[num] = right[j]
+            j += 1
+        num += 1
+    while i < len(left):
+        arr[num] = left[i]
+        i += 1
+        num += 1
+    while j < len(right):
+        arr[num] = right[j]
+        j += 1
+        num += 1
+
+
+if __name__ == "__main__":
+    arr = [7, 4, 9, 8, 1]
+    merge_sort(arr)
+    print(arr)
