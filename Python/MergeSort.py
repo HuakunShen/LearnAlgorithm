cb835d6fea38d9748e07670803f5247ef37dbe6d
Huakun Shen <HuakunShen@users.noreply.github.com>
2019-06-28 12:38:12 -0400

    added datastructure file

--- Python/MergeSort.py
+++ Python/MergeSort.py
@@ -1,18 +1,23 @@
 from IsSorted import *
 def merge_sort(arr):
+	''' Merge arr in non-decreasing order '''
+	# Base Case
     if len(arr) <= 1:
         return
+    
+    # Divide and Conquer
     mid = len(arr) // 2
     left = arr[:mid]
     right = arr[mid:]
-
     merge_sort(left)
     merge_sort(right)
 
+	# Merge (Generate) final solution
     merge(arr, left, right)
 
 
 def merge(arr, left, right):
+	''' Merge Two Sorted Array into One Sorted Array '''
     i = j = num = 0
     while i < len(left) and j < len(right):
         if left[i] < right[j]:
